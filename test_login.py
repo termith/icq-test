ea44c46f3f2095a75551fd63ac2eef40880d34ac
termith <dmitdemidov@gmail.com>
2015-11-02 11:04:34 +0300

    refactor test logic

--- test_login.py
+++ test_login.py
@@ -7,23 +7,24 @@ class TestIcqLogin(unittest.TestCase):
     def setUp(self):
         self.client = IcqTestClient('291140261', 'password')
 
+    def test_server_available(self):
+        login_status, _ = self.client.do_login()
+        assert login_status != const.INTERNAL_ERROR_CODE, 'Sever returns internal error'
+
     def test_correct_credentials(self):
         login_status, login_data = self.client.do_login()
-        assert login_status != const.INTERNAL_ERROR_CODE, 'Internal error! See log for more details'
         assert login_status == const.EXTERNAL_LOGIN_OK, 'Cannot login with correct credentials'
         assert login_data.get('loginId', '') == self.client.login, 'Wrong login id returned in response'
 
     def test_incorrect_credentials(self):
         self.client.password = 'wrongpassword'
         login_status, _ = self.client.do_login()
-        assert login_status != const.INTERNAL_ERROR_CODE, 'Internal error! See log for more details'
         assert login_status == const.EXTERNAL_LOGIN_ERROR, 'Incorrect login status'
 
     def test_empty_credentials(self):
         self.client.login = str()
         self.client.password = str()
         login_status, _ = self.client.do_login()
-        assert login_status != const.INTERNAL_ERROR_CODE, 'Internal error! See log for more details'
         assert login_status == const.EXTERNAL_LOGIN_MISSING, 'Incorrect login status'
 
 
